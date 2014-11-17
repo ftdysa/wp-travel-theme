4d69ebc1699dbb57e5c37b1dabca844d7f8eff0c
Fred Dysart <ftdysa@gmail.com>
2014-11-16 23:02:22 -0500

    Added: map and gps coordinate handling

--- map.php
+++ map.php
@@ -18,14 +18,37 @@ get_header();
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         map = new google.maps.Map(document.getElementById('google-map'), options);
+        loadPoints(map);
     }
     google.maps.event.addDomListener(window, 'load', initialize);
 
-    function loadPoints() {
-        for (var i = 0; i < data.length; i++) {
-            var latLng = new google.maps.LatLng(data[i][0], data[i][1]);
-            var marker = new google.maps.Marker({position: latLng, map: map});
-        }
+    function trim(str) {
+        return str.replace(/^\s+|\s+$/g, '');
+    }
+    function loadPoints(map) {
+        $.get("<?php echo get_template_directory_uri(); ?>/locations.csv",
+            function(data) {
+                var lines = data.split(/\n/);
+                for (var i = 0; i < lines.length; i++) {
+                    if (!lines[i]) continue;
+                    var vals = lines[i].split(':');
+                    var latLng = vals[1].split(',');
+
+                    var latLngObj = new google.maps.LatLng(parseFloat(trim(latLng[0])), parseFloat(trim(latLng[1])));
+                    var marker = new google.maps.Marker({
+                        position: latLngObj, 
+                        map: map,
+                        title: vals[0]
+                    });
+
+                    var infoWindow = new google.maps.InfoWindow({content: vals[0]});
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infoWindow.setContent(this.title);
+                        infoWindow.open(map, this); 
+                    });
+                }
+            }
+        );
     }
 </script>
 </div>
